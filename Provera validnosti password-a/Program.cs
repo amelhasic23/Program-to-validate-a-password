3bb99a36243feea7dd709ad338c3cd95670b85e5
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 09:14:54 +0100

    Commit

---
+++ Provera validnosti password-a/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite password: ");
+        string password = Console.ReadLine();
+
+        if (ValidirajPassword(password))
+        {
+            Console.WriteLine("Password je validan.");
+        }
+        else
+        {
+            Console.WriteLine("Password nije validan. Mora imati najmanje 7 znakova i sadržavati barem jedan '!' ili '?'.");
+        }
+    }
+
+    static bool ValidirajPassword(string password)
+    {
+        if (password.Length < 7)
+            return false;
+
+        if (!password.Contains("!") && !password.Contains("?"))
+            return false;
+
+        return true;
+    }
+}
+
